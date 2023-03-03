b75da710072ffed0e9b99ceb20562eb99f726b0b
Ashraful Alam Limon <ashrafullimon38@gmail.com>
2023-03-03 11:40:19 +0600

    cart item add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart}= props;
+
+    console.log(cart);
+    
+    let total = 0;
+    for(const product of cart){
+        total= total + product.price;
+    };
+
     return (
-        <div>
-            <h4>Order Summary</h4>
+        <div className='Cart'>
+            <h3 style={{textAlign:'center'}}>Order Summary</h3>
             <p>selected items:{cart.length}</p>
+            <p>Total Price:{total}</p>
+            <p>Total Shipping Charge:</p>
+            <p>Tax:</p>
+            <h4>Grand Total:</h4>
         </div>
     );
 };
